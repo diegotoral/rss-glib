a83d8d1ea4969fd5d1f6d4a70830f05c083ddde8
Christian Hergert <chris@dronelabs.com>
2008-06-25 23:24:11 -0700

    update test

--- tests/test-01.c
+++ tests/test-01.c
@@ -55,39 +55,51 @@ main (int argc, char **argv)
 
 	g_object_get (document, "title", &str, NULL);
 	g_print ("Title:        %s\n", str);
+	g_free (str);
 
 	g_object_get (document, "guid", &str, NULL);
 	g_print ("Guid:         %s\n", str);
+	g_free (str);
 
 	g_object_get (document, "description", &str, NULL);
 	g_print ("Description:  %s\n", str);
+	g_free (str);
 
 	g_object_get (document, "link", &str, NULL);
 	g_print ("Link:         %s\n", str);
+	g_free (str);
 
 	g_object_get (document, "language", &str, NULL);
 	g_print ("Language:     %s\n", str);
+	g_free (str);
 
 	g_object_get (document, "rating", &str, NULL);
 	g_print ("Rating:       %s\n", str);
+	g_free (str);
 
 	g_object_get (document, "copyright", &str, NULL);
 	g_print ("Copyright:    %s\n", str);
+	g_free (str);
 
 	g_object_get (document, "pub_date", &str, NULL);
 	g_print ("Published:    %s\n", str);
+	g_free (str);
 
 	g_object_get (document, "editor", &str, NULL);
 	g_print ("Editor:       %s\n", str);
+	g_free (str);
 
 	g_object_get (document, "generator", &str, NULL);
 	g_print ("Generator:    %s\n", str);
+	g_free (str);
 
 	g_object_get (document, "image-title", &str, NULL);
 	g_print ("Image Title:  %s\n", str);
+	g_free (str);
 
 	g_object_get (document, "image-url", &str, NULL);
 	g_print ("Image Url:    %s\n", str);
+	g_free (str);
 
 	list = rss_document_get_categories (document);
 	if (list != NULL) {
@@ -102,6 +114,9 @@ main (int argc, char **argv)
 		g_list_foreach (list, (GFunc) print_item, NULL);
 		g_list_free (list);
 	}
+
+	g_object_unref (document);
+	g_object_unref (parser);
 
 	g_print ("\n");
 	return EXIT_SUCCESS;
