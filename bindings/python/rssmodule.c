a9f361f1bfd09e2928b932c28e7c341167d56d2a
Christian Hergert <chergert@chergert-desktop.(none)>
2008-10-31 20:43:46 -0700

    Start on python bindings!
    2008-10-31	Christian Hergert	<chris@dronelabs.com>
    
    	* .gitignore: Update for python bindings.
    	* Makefile.am:
    	* acinclude.m4: Add macros to aide in checking for python.
    	* bindings/Makefile.am:
    	* bindings/python/Makefile.am:
    	* bindings/python/rss.defs:
    	* bindings/python/rss.override:
    	* bindings/python/rssmodule.c: New python binding for rss-glib.
    	* bindings/vala/Makefile.am:
    	* bindings/vala/rss-glib-1.0.vapi:
    	* bindings/vala/rss-test.vala: Moved from vala/
    	* configure.ac: Add checking for python to enable python bindings.

---
+++ bindings/python/rssmodule.c
@@ -0,0 +1,48 @@
+/* rssmodule.c
+ *
+ * Copyright (C) 2008 Christian Hergert <chris@dronelabs.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston MA 
+ * 02110-1301 USA
+ */
+
+#ifndef HAVE_CONFIG_H
+#	include "../config.h"
+#endif
+
+#include <Python.h>
+#include <pygobject.h>
+#include "../../rss-glib/rss-glib.h"
+
+void pyrss_register_classes (PyObject *d);
+extern PyMethodDef pyrss_functions[];
+
+DL_EXPORT (void)
+initrss (void)
+{
+	PyObject *m, *d;
+
+	PyEval_InitThreads ();
+	init_pygobject ();
+
+	m = Py_InitModule ("rss", pyrss_functions);
+	d = PyModule_GetDict (m);
+
+	pyrss_register_classes (d);
+
+	if (PyErr_Occurred ())
+		Py_FatalError ("Error initializing module rss");
+}
+
